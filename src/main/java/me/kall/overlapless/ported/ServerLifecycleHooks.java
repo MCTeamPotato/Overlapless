d19e999318560b71906ba6bd8ac64867d63ddb6a
狂想ThePotato <110756250+Kasualix@users.noreply.github.com>
2026-03-06 12:41:45 +0800

    minor cleanup

--- src/main/java/me/kall/overlapless/ported/ServerLifecycleHooks.java
+++ src/main/java/me/kall/overlapless/ported/ServerLifecycleHooks.java
@@ -5,12 +5,6 @@ import net.minecraft.server.MinecraftServer;
 public class ServerLifecycleHooks {
     private static MinecraftServer currentServer;
 
-//    public static void init() {
-//        ServerLifecycleEvents.SERVER_STARTING.register(server -> {
-//            currentServer = server;
-//        });
-//    }
-
     public static void setCurrentServer(MinecraftServer server) {
         currentServer = server;
     }
